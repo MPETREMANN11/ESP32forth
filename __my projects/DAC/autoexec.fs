2f22bc23d2eec36db7654a840a9ea126d999f68b
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-11-08 16:44:49 +0100

    Create autoexec.fs

---
+++ __my projects/DAC/autoexec.fs
@@ -0,0 +1,24 @@
+RECORDFILE /spiffs/autoexec.fs
+create crlf 13 C, 10 C,
+: RECORDFILE  ( "filename" "filecontents" "<EOF>" -- )
+    bl parse
+    W/O CREATE-FILE throw >R
+    BEGIN
+        tib #tib accept
+        tib over
+        S" <EOF>" startswith?
+        DUP IF
+            swap drop
+        ELSE
+            swap
+            tib swap
+            R@ WRITE-FILE throw
+            crlf 1+ 1 R@ WRITE-FILE throw
+        THEN
+    UNTIL
+    R> CLOSE-FILE throw
+;
+: MAIN
+    s" /spiffs/main.fs"       included
+  ;
+<EOF>
