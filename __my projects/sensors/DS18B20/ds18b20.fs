e159736950843fd3e9846712d54f597747d66e41
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-12-05 12:02:57 +0100

    Add files via upload

---
+++ __my projects/sensors/DS18B20/ds18b20.fs
@@ -0,0 +1,11 @@
+\ *********************************************************************
+\ DS18B20 teperature sensor
+\    Filename:      config.fs
+\    Date:          05 dec 2023
+\    Updated:       05 dec 2023
+\    File Version:  1.0
+\    MCU:           ESP32-WROOM-32
+\    Forth:         ESP32forth all versions 7.x++
+\    Copyright:     Marc PETREMANN
+\    GNU General Public License
+\ *********************************************************************
---
+++ __my projects/sensors/DS18B20/tests.fs
@@ -0,0 +1,19 @@
+\ *********************************************************************
+\ DS18B20 teperature sensor / unit tests
+\    Filename:      tests.fs
+\    Date:          05 dec 2023
+\    Updated:       05 dec 2023
+\    File Version:  1.0
+\    MCU:           ESP32-WROOM-32
+\    Forth:         ESP32forth all versions 7.x++
+\    Copyright:     Marc PETREMANN
+\    GNU General Public License
+\ *********************************************************************
+
+
+
+
+
+
+
+
