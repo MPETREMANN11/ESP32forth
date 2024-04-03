f774f320023ae8bdde8bb775bfc65bb76f85c285
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2024-04-03 17:36:40 +0200

    Add files via upload

---
+++ __sandbox/NEXUS SENSOR/apds9960.fs
@@ -0,0 +1,16 @@
+\ *********************************************************************
+\ BME688
+\    Filename:      apds9960.fs
+\    Date:          03 apr. 2024
+\    Updated:       03 apr. 2024
+\    File Version:  0.0
+\    MCU:           ESP32 all models
+\    Forth:         ESP32forth all versions 7.x++
+\    Copyright:     ---
+\    GNU General Public License
+\ *********************************************************************
+
+
+\ set I2S address for APDS9960
+0 value APDS9960_I2S_ADDRESS
+
---
+++ __sandbox/NEXUS SENSOR/pcf8523.fs
@@ -0,0 +1,16 @@
+\ *********************************************************************
+\ BME688
+\    Filename:      pcf8523.fs
+\    Date:          03 apr. 2024
+\    Updated:       03 apr. 2024
+\    File Version:  0.0
+\    MCU:           ESP32 all models
+\    Forth:         ESP32forth all versions 7.x++
+\    Copyright:     ---
+\    GNU General Public License
+\ *********************************************************************
+
+
+\ set I2S address for pcf8523
+0 value PCF8523_I2S_ADDRESS
+
